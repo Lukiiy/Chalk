0c8c17214a3d3db172897868bc1aa42675e8d4e8
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-20 23:26:16 -0300

    Out of Bounds Inland elimination!
    * Removed water check from the Out of Bounds patch, meaning that you can
      also explode if you hit ur ball off bounds inland.

--- src/Chalk/instaKill.cs
+++ src/Chalk/instaKill.cs
@@ -14,14 +14,14 @@ public class InstaKill
     {
         if (!toggled || __instance == null || !__instance.isServer) return;
 
-        LevelBoundsTracker tracker = __instance.AsEntity.LevelBoundsTracker;
-
-        bool inWater = (tracker.AuthoritativeBoundsState.HasState(BoundsState.InMainOutOfBoundsHazard) && (MainOutOfBoundsHazard.Type == OutOfBoundsHazard.Water || MainOutOfBoundsHazard.Type == OutOfBoundsHazard.Fog)) || (tracker.AuthoritativeBoundsState.HasState(BoundsState.InSecondaryOutOfBoundsHazard) && tracker.CurrentSecondaryHazardLocalOnly != null && tracker.CurrentSecondaryHazardLocalOnly.Type == OutOfBoundsHazard.Water);
-        if (!inWater) return;
-
         var playerInfo = __instance.Networkowner?.PlayerInfo;
         if (playerInfo == null) return;
 
+        Eliminate(playerInfo);
+    }
+
+    public static void Eliminate(PlayerInfo playerInfo)
+    {
         VfxManager.ServerPlayPooledVfxForAllClients(VfxType.MineExplosion, playerInfo.transform.position, Quaternion.identity);
         playerInfo.AsGolfer.ServerEliminate(EliminationReason.OutOfBounds);
     }
