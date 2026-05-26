3613864d1c97d6ea0e831e568eeecfaab1d8e986
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-26 09:46:28 -0300

    Setup config entries

--- src/Chalk/Chalk.cs
+++ src/Chalk/Chalk.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using Chalk.utils;
 using HarmonyLib;
 using UnityEngine;
@@ -8,13 +9,27 @@ namespace Chalk;
 [BepInAutoPlugin]
 public partial class Chalk : BaseUnityPlugin
 {
+    internal static ConfigEntry<bool> windBursts = null!;
+    internal static ConfigEntry<bool> minedLootboxes = null!;
+    internal static ConfigEntry<bool> holeMinefield = null!;
+    internal static ConfigEntry<bool> ballPush = null!;
+    internal static ConfigEntry<bool> instaKills = null!;
+    internal static ConfigEntry<bool> extraLoots = null!;
+
     private void Awake()
     {
         Harmony harmony = new(Info.Metadata.GUID);
-        harmony.PatchAll();
 
-        WindBursts.Start();
+        windBursts = Config.Bind("Chalk", "Wind Bursts", true, "Toggles random bursts of wind mid match");
+        minedLootboxes = Config.Bind("Chalk", "Mined Lootboxes", true, "Some loot boxes spawn with a land mine below");
+        holeMinefield = Config.Bind("Chalk", "Hole Minefield", true, "Places 4 mines near the hole");
+        ballPush = Config.Bind("Chalk", "Ball Pushing", true, "Allows players to slowly push others' balls when close");
+        instaKills = Config.Bind("Chalk", "Instant Kills", true, "Throwing a ball out of bounds makes you explode");
+        extraLoots = Config.Bind("Chalk", "Extra Loot", true, "Knocking out players gives you loot");
+
+        harmony.PatchAll();
         CourseManager.MatchStateChanged += OnMatchStateChanged;
+        WindBursts.Start();
     }
 
     private void Update()
