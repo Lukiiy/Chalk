42b459c66ea263436dbd9e5f04d6db6751d5a0eb
Lukiiy <lukiiy@proton.me>
2026-07-17 21:08:03 -0300

    Remove velocity detection with Chalk spawned mines.
    * Added custom ChalkMine component
    * Added tagging patch (tag a mine as chalk spawned)
    * Added velocity check patch

--- src/Chalk/extraMines.cs
+++ src/Chalk/extraMines.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Reflection;
+using System.Reflection.Emit;
+using Chalk.utils;
 using HarmonyLib;
 using Mirror;
 using UnityEngine;
@@ -10,6 +12,8 @@ namespace Chalk;
 public class ExtraMines
 {
     private static readonly MethodInfo ExplodeMethod = AccessTools.Method(typeof(Landmine), "ServerExplode", []);
+    private static readonly MethodInfo WillBeReflectedMethod = AccessTools.Method(typeof(Landmine), "WillBeReflectedOnCollision", [typeof(Entity)]);
+    private static readonly MethodInfo IsTeammateMethod = AccessTools.Method(typeof(Landmine), "IsTeammateOfOwner", [typeof(Entity)]);
 
     public static bool seeded = true;
 
@@ -50,6 +54,8 @@ public class ExtraMines
         foreach (var off in offsets) Chalk.SpawnServerMine(flagPos + off + new Vector3(0f, .1f, 0f));
     }
 
+    // land mine patches
+
     [HarmonyPatch(typeof(Landmine), "OnCollisionEnter")]
     [HarmonyPostfix]
     private static void MineChain(Landmine __instance, Collision collision)
@@ -61,4 +67,35 @@ public class ExtraMines
         ExplodeMethod.Invoke(__instance, null);
         ExplodeMethod.Invoke(otherMine, null);
     }
+
+    // Tags mines spawned by Chalk with the custom component.
+    [HarmonyPatch(typeof(Landmine), "ServerInitialize")]
+    [HarmonyPostfix]
+    private static void Tag(Landmine __instance, PlayerInventory owner)
+    {
+        if (owner == FakeInventory.Get()) __instance.gameObject.AddComponent<ChalkMine>();
+    }
+
+    // Replaces the vanilla velocity check with ChalkThreshold to override mine detection for mines spawned by Chalk.
+    [HarmonyPatch(typeof(Landmine), "ProcessCollidersInWarningRange")]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> VelocityCheck(IEnumerable<CodeInstruction> instructions)
+    {
+        var getter = AccessTools.PropertyGetter(typeof(ItemSettings), nameof(ItemSettings.LandmineDetectionMinSpeedSquared));
+        var helper = AccessTools.Method(typeof(ExtraMines), nameof(ChalkThreshold));
+
+        // Replace the vanilla getter with ChalkThreshold.
+        foreach (var code in instructions)
+        {
+            yield return code;
+
+            if (code.Calls(getter))
+            {
+                yield return new CodeInstruction(OpCodes.Ldarg_0);
+                yield return new CodeInstruction(OpCodes.Call, helper);
+            }
+        }
+    }
+
+    private static float ChalkThreshold(float vanilla, Landmine mine) => mine.TryGetComponent<ChalkMine>(out _) ? 0f : vanilla; // This overrides the vanilla threshold to 0 for mines spawned by Chalk.
 }
---
+++ src/Chalk/utils/chalkMine.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+namespace Chalk.utils;
+
+public class ChalkMine : MonoBehaviour {}
